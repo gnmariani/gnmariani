973e34c59f99e6ab7f21e0c64942534cb231d9be
Gina Mariani <gina.nvillarreal@gmail.com>
2023-11-13 17:20:24 -0500

    Add new files and data folders

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "bright-white": "#F8F2F1",
+      "abaddon-black": "#211E22",
+      "jade-powder": "#2BAA6D",
+      "fall-shill": "#E2DDDC",
+    },
   },
   plugins: [],
 };
