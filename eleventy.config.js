adf6d55dcb6bc3345ba470e242b98d8fc3832733
Gina Mariani <gina.nvillarreal@gmail.com>
2023-11-13 13:01:50 -0500

    shortcodes for images and year. add images lib

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
+const Image = require("@11ty/eleventy-img");
+const date = new Date();
 const now = String(Date.now());
+const fullYear = date.getFullYear();
 
 module.exports = function (eleventyConfig) {
   // Input directory: src
@@ -7,11 +10,32 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/css/tailwind.config.js");
   eleventyConfig.addWatchTarget("src/css/tailwind.css");
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   eleventyConfig.addShortcode("version", function () {
     return now;
   });
+  eleventyConfig.addShortcode("year", function () {
+    return fullYear;
+  });
+
+  async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(`./src${src}`, {
+      widths: [300, 800, null],
+      formats: ["avif", "jpeg"],
+      urlPath: "/assets/",
+      outputDir: "./dist/assets/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  }
 
   return {
     markdownTemplateEngine: "njk",
