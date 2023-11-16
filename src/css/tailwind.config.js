f0c83b6e779b9cf1e8b70bc59f526d1e6a347a58
Gina Mariani <gina.nvillarreal@gmail.com>
2023-11-16 18:56:08 -0500

    add css grid for page layout

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: "auto 1fr auto", //TODO: MODIFY THIS TO INCREASE SIZE OF FOOTER (SPECIALLY FOR LARGER SCREENS)
+      },
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
       white: "#ffffff",
+      green: colors.emerald,
+      orange: colors.orange,
+      red: colors.red,
+      purple: colors.violet,
       "bright-white": "#F8F2F1",
       "abaddon-black": "#211E22",
       "jade-powder": "#2BAA6D",
       "fall-shill": "#E2DDDC",
+    },
+    fontFamily: {
+      sans: ["DM Sans", "Helvetica", "Arial", "sans-serif", "system-ui"],
     },
   },
   plugins: [],
